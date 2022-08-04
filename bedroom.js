f0cac213fa08912fd49a4c9d0d81fa8ac3be98ef
AimeeR74655 <82007449+AimeeR74655@users.noreply.github.com>
2022-08-04 15:26:28 -0400

    Add files via upload

---
+++ bedroom.js
@@ -0,0 +1,37 @@
+img="";
+status="";
+
+
+function preload()
+{
+    img = loadImage("bedroom.jpeg");
+}
+function setup()
+{
+    canvas= createCanvas( 640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded()
+ {
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+ }
+
+ function draw()
+ {
+     image(img, 0, 0, 640, 420);
+ }
+
+ function gotResult(error, results)
+ {
+     if(error)
+     {
+         console.log(error);
+     }
+     console.log(results);
+     
+ }
